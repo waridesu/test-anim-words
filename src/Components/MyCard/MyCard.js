ca2480256c38ed890739a9374d2c1090416e856a
Denis <waridesu@gmail.com>
2020-10-07 22:45:21 +0300

    variable

--- src/Components/MyCard/MyCard.js
+++ src/Components/MyCard/MyCard.js
@@ -2,13 +2,13 @@ import React, {useState} from 'react';
 import s from './MyCard.module.scss'
 
 const MyCard = props => {
-    let [done, setDone] = useState(null)
+    let [isDone, setIsDone] = useState(null)
     const handleCardClick = () => {
-        if (!done) setDone(done = true);
-        else setDone(done = false);
+        if (!isDone) setIsDone(isDone = true);
+        else setIsDone(isDone = false);
     }
     return <div onClick={handleCardClick} className={s.wordWrap}>
-        <div className={`${done === true && s.anim} ${done ? s.true : s.false}`}>{done ? props.rus : props.eng}</div>
+        <div className={`${isDone && s.anim} ${isDone ? s.true : s.false}`}>{isDone ? props.rus : props.eng}</div>
     </div>
 }
 export default MyCard;

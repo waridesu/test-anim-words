59d37c76ca71c9068273106ad6cec7ebe07e7496
Denis <waridesu@gmail.com>
2020-10-07 19:54:33 +0300

    animation

--- src/Components/MyCard/MyCard.js
+++ src/Components/MyCard/MyCard.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import s from './MyCard.module.scss'
-import cl from 'classnames'
 
 const MyCard = props => {
     let [done, setDone] = useState(null)
@@ -8,9 +7,8 @@ const MyCard = props => {
         if (!done) setDone(done = true);
         else setDone(done = false);
     }
-    return <div onClick={handleCardClick}
-                className={s.wordWrap}>
-        {done ? <div className={cl({[s.true]: done,[s.anim]: done === true})}>{props.rus}</div> : <div className={cl({[s.false]: !done,[s.anim]: done === false})}>{props.eng}</div>}
+    return <div onClick={handleCardClick} className={s.wordWrap}>
+        <div className={`${done === true && s.anim} ${done ? s.true : s.false}`}>{done ? props.rus : props.eng}</div>
     </div>
 }
 export default MyCard;
